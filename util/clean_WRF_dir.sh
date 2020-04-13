46e58d02c548458377dadacab7ad3609e8559c86
jesusff <Jesus.Fernandez@unican.es>
2020-04-13 18:01:34 +0200

    Update gitignore and move SOIL:* on cleaning

--- util/clean_WRF_dir.sh
+++ util/clean_WRF_dir.sh
@@ -2,7 +2,7 @@ tag=$1
 target="WRF.${tag}"
 mkdir $target || exit
 
-mv WRF/FX* WRF/FILE* WRF/PRES* WRF/GRIBFILE.* $target/
+mv WRF/FX* WRF/FILE* WRF/SOIL\:* WRF/PRES* WRF/GRIBFILE.* $target/
 mv WRF/wrfrst* WRF/wrfout* WRF/wrfbdy* WRF/wrfinput* WRF/wrflowinp* $target/
 mv WRF/ecmwf_coeffs WRF/met_em.* WRF/namelist.output WRF/rsl.* $target/
 mv WRF/ungrib.out* WRF/ungrib.log WRF/logfile.log WRF/metgrid.log $target/
