abd4719fa1925ffc60cd146ba5b87a325a563c28
zequihg50 <ezequiel.cimadevilla@unican.es>
2020-04-23 09:32:38 +0200

    Updated conda recipe

--- conda/build.sh
+++ conda/build.sh
@@ -1,11 +1,12 @@
 mkdir -p ${PREFIX}/bin
-cp ${SRC_DIR}/preprocessor.ESGF ${PREFIX}/bin/bctool
-chmod a+x ${PREFIX}/bin/bctool
+cp ${SRC_DIR}/preprocessor.ESGF ${PREFIX}/bin/preprocessor.ESGF
+chmod a+x ${PREFIX}/bin/preprocessor.ESGF
+ln -s ${PREFIX}/bin/preprocessor.ESGF ${PREFIX}/bin/bctool
 
 mkdir -p ${PREFIX}/share/bctool
 cp ${SRC_DIR}/BCtable.* ${PREFIX}/share/bctool
 
 mkdir -p ${PREFIX}/share/bctool/data-download
 cp ${SRC_DIR}/data-download/* ${PREFIX}/share/bctool/data-download
-cp ${SRC_DIR}/data-download/download ${PREFIX}/bin/bctool-download
+cp ${SRC_DIR}/data-download/bctool-download ${PREFIX}/bin/bctool-download
 chmod a+x ${PREFIX}/bin/bctool-download
