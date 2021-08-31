a93e0ec47026dd3ebda10b4c135c74d93da128fd
Yoselita <milovacj@unican.es>
2021-08-31 03:07:01 +0200

    Vtable for CanESM2 model adjusted to be able to read the soil variabels at the lowest soil layer when running ungrib.exe.

--- templates/Vtable.CanESM2.ml
+++ templates/Vtable.CanESM2.ml
@@ -5,11 +5,11 @@ Param| Type |Level1|Level2| Name     |  Units   | Description                   
   33 | 109  |   *  |      | UU       | m s-1    | U                                        |
   34 | 109  |   *  |      | VV       | m s-1    | V                                        |
   52 | 109  |   *  |      | SPECHUMD | kg kg-1  | Specific Humidity                        |
-  11 | 105  |   0  |      | TT       | K        | Temperature                              | At 2 m
-  52 | 105  |   0  |      | SPECHUMD | kg kg-1  |                                          | At 2 m
-     | 105  |   0  |      | RH       | %        | Relative Humidity at 2 m                 | At 2 m
-  33 | 105  |   0  |      | UU       | m s-1    | U                                        | At 10 m
-  34 | 105  |   0  |      | VV       | m s-1    | V                                        | At 10 m
+  11 | 105  |   2  |      | TT       | K        | Temperature                              | At 2 m
+  52 | 105  |   2  |      | SPECHUMD | kg kg-1  |                                          | At 2 m
+     | 105  |   2  |      | RH       | %        | Relative Humidity at 2 m                 | At 2 m
+  33 | 105  |  10  |      | UU       | m s-1    | U                                        | At 10 m
+  34 | 105  |  10  |      | VV       | m s-1    | V                                        | At 10 m
  172 |  1   |   0  |      | LANDSEA  | 0/1 Flag | Land/Sea flag                            |
  129 |  1   |   0  |      | SOILHGT  | m        | Terrain field of source analysis         |
   31 |  1   |   0  |      | SEAICE   | 0/1 Flag | Sea-Ice-Flag                             |
@@ -20,8 +20,8 @@ Param| Type |Level1|Level2| Name     |  Units   | Description                   
   37 |  1   |   0  |      | SST      | K        | Sea-Surface Temperature                  |
  139 | 112  |   0  |  10  | ST000010 | K        | T of 0-10 cm ground layer                |
  170 | 112  |  10  |  40  | ST010040 | K        | T of 10-40 cm ground layer               |
- 183 | 112  |  40  | 255  | ST040255 | K        | T of 40-405 cm ground layer              |
+ 183 | 112  |  40  | 149  | ST040405 | K        | T of 40-405 cm ground layer              |
   39 | 112  |   0  |  10  | SM000010 | fraction | Soil moisture of 0-10 cm ground layer    |
   40 | 112  |  10  |  40  | SM010040 | fraction | Soil moisture of 10-40 cm ground layer   |
-  41 | 112  |  40  | 255  | SM040255 | fraction | Soil moisture of 40-405 cm ground layer  |
+  41 | 112  |  40  | 149  | SM040405 | fraction | Soil moisture of 40-405 cm ground layer  |
 -----+------+------+------+----------+----------+------------------------------------------+
